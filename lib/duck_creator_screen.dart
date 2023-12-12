9607c074b037bf3481985ce0270083888457be5b
eduandreh <eduandrehardoy@gmail.com>
2023-12-12 21:52:05 +0100

    Login y Registro con Firebase

---
+++ lib/duck_creator_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class DuckCreator extends StatefulWidget {
+  const DuckCreator({super.key});
+
+  @override
+  State<DuckCreator> createState() => _DuckCreatorState();
+}
+
+class _DuckCreatorState extends State<DuckCreator> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/signin_screen.dart
@@ -0,0 +1,125 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:proyecto_ddm2/duck_creator_screen.dart';
+import 'package:proyecto_ddm2/signup_register.dart';
+
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+TextEditingController _passwordTextController = TextEditingController();
+TextEditingController _emailTextController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 70),
+              Image.network(
+            'https://firebasestorage.googleapis.com/v0/b/duffy-264e6.appspot.com/o/icons%2FLogoDUFFY.png?alt=media&token=bb619868-3f5a-4724-a9a6-794f03dd2127',
+            height: 100,
+            width: 100,        
+          ),
+            const Text(
+              'Bienvenid@!',
+              style: TextStyle(
+                fontFamily: 'Mulish',
+                fontSize: 36,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            const Text(
+              'Logueate para ver a tu Duffy!',
+              style: TextStyle(
+                fontFamily: 'Mulish',
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _emailTextController,
+              decoration: const InputDecoration(
+                hintText: 'Correo electrónico',
+                suffixIcon: Icon(Icons.email),
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            TextField(
+              obscureText: true,
+              controller: _passwordTextController,
+              decoration: const InputDecoration(
+                hintText: 'Contraseña',
+                suffixIcon: Icon(Icons.lock),
+                // filled: true,
+                // fillColor: Color.fromRGBO(221, 138, 41, 0.1),
+              ),
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+               onPressed: () {
+                FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: _emailTextController.text,
+                  password: _passwordTextController.text,
+                ).then((value) => Navigator.push(context, MaterialPageRoute(builder: (context) => const DuckCreator()))).catchError((error) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('No se pudo iniciar sesión'),
+                    ),
+                  );
+                 });
+                
+               },
+               style: ElevatedButton.styleFrom(
+                 primary: const Color.fromRGBO(221, 138, 41, 1),
+                 onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                   
+               ),
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                alignment: Alignment.center,
+                //width: MediaQuery.of(context).size.width * 0.5,
+                child: const Text(
+                  'Entrar >',
+                  style: TextStyle(
+                    fontFamily: 'Mulish',
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SignUpScreen(),
+                  ),
+                );
+              },
+              child: const Text('Nuevo en Duffy? Regístrate'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
