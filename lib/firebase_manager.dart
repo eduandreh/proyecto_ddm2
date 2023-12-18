aad781a2f8e3eefd1c67775105a0fe2a6063b2c1
superfer2002 <fporcel2002@gmail.com>
2023-12-18 13:13:19 +0100

    Placeholder for Shop and creation of class accessories for Duffy and shop objects - Fernando

--- lib/firebase_manager.dart
+++ lib/firebase_manager.dart
@@ -2,53 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
+import 'Shop.dart';
+
 class FirebaseManager {
 
   FirebaseFirestore db = FirebaseFirestore.instance;
   FirebaseAuth auth = FirebaseAuth.instance;
 
   FirebaseStorage storage = FirebaseStorage.instance;
-
-  /*Future<String> checkUser(mail, password) async {
-    String loginInfo = await signUp(mail, password);
-    return loginInfo;
-  }*/
-
-  //signUp
-  /*Future<String> signUp(mail, password) async {
-    //try catch
-    try {
-      await auth.createUserWithEmailAndPassword(
-        email: mail,
-        password: password,
-      );
-      CollectionReference usersCollection = db.collection('users');
-      usersCollection
-          .doc(auth.currentUser?.uid ?? '')
-          .set({'mail': mail}).then((value) => print("Users Document Added"));
-    } on FirebaseAuthException catch (e) {
-      print(e.code);
-      if (e.code == 'weak-password') {
-        return 'The password provided is too weak.';
-      } else if (e.code == 'email-already-in-use') {
-        return logIn(mail, password);
-      }
-    }
-    return '';
-  }
-
-  //logIn
-  Future<String> logIn(mail, password) async {
-    try {
-      UserCredential user = await auth.signInWithEmailAndPassword(
-          email: mail, password: password);
-    } on FirebaseAuthException catch (e) {
-      print(e.code);
-      return 'Wrong password provided for that user.';
-    }
-    return '';
-  }*/
-
 
   Future<List<String>> getImagesURL(folderPath) async {
 
@@ -67,5 +28,35 @@ class FirebaseManager {
 
     return urlList;
   }
+
+  Future<List<Shop>> getShop() async {
+    List<Shop> shopObjects = [];
+
+    final ref = db.collection("shop").withConverter(
+        fromFirestore: Shop.fromFirestore,
+        toFirestore: (Shop shop, _) => shop.toFirestore());
+
+    var querySnapshot = await ref.get();
+
+    for (var snapshot in querySnapshot.docs) {
+      if (snapshot != null) {
+        shopObjects.add(snapshot.data());
+      }
+    }
+
+    return shopObjects;
+  }
+
+  void updateDuckinessWithSwipes() {
+    //get ducks duckiness
+
+  }
+
+  void getCoordsByCity(){
+
+  }
+//get coords by city name
+
+
 
 }
---
+++ lib/shop_screen.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_ddm2/Shop.dart';
+import 'firebase_manager.dart';
+
+class ShopScreen extends StatefulWidget {
+  const ShopScreen({super.key});
+
+  @override
+  State<ShopScreen> createState() => _ShopScreen();
+}
+
+class _ShopScreen extends State<ShopScreen> {
+  int _counter = 950;
+
+  List<Shop> shopObjects = [];
+  FirebaseManager fManager = FirebaseManager();
+
+  List<String> weatherIcons = [
+    'assets/weather/1rain_icon.png',
+    'assets/weather/2hot_icon.png',
+    'assets/weather/3snow_icon.png',
+    'assets/weather/4wind_icon.png',
+    'assets/weather/5tornado_icon.png',
+    'assets/weather/6sun_icon.png',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    getInfo();
+  }
+
+  void getInfo() async {
+    //shopObjects = await fManager.getShop();
+    shopObjects = [];
+    setState(() {});
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter = _counter - 150;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            centerTitle: true,
+            leading: BackButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              color: Colors.black,
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const SizedBox(width: 30),
+                Transform.rotate(
+                  angle: 3.14159 / 2,
+                  child: const Text(
+                    'M',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff9C4615),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 2),
+                Text(
+                  "$_counter",
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            )),
+        body: Center(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Compremos!",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32,
+                      )),
+                  Text("que llevará tu Duffy?",
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 16,
+                      )),
+                ],
+              ),
+              const SizedBox(height: 32),
+              Visibility(
+                visible: shopObjects.isEmpty,
+                child: Image(
+                  image: Image.asset(  //placeholder
+                      "assets/weather/6sun_icon.png")
+                      .image,
+                  width: 400,
+                ),
+              ),
+              Visibility(
+                visible: shopObjects.isNotEmpty,
+                child: Expanded(
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 1,
+                            mainAxisSpacing: 12.0,
+                            crossAxisSpacing: 1.0,
+                            childAspectRatio: 4),
+                    itemCount: 5,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        elevation: 0,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image(
+                              image: shopObjects.isNotEmpty
+                                  ? NetworkImage(shopObjects[index].image)
+                                  : const NetworkImage(
+                                      "https://firebasestorage.googleapis.com/v0/b/duffy-264e6.appspot.com/o/shop_accessories%2F1umbrella.png?alt=media&token=57da99d1-bbce-43f4-a435-3b1b5ac4dd9e"),
+                              width: 101,
+                            ),
+                            const SizedBox(width: 64),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    const SizedBox(width: 12),
+                                    Image(
+                                      //weather icon
+                                      image: Image.asset(weatherIcons[index])
+                                          .image,
+                                      width: 24,
+                                    ),
+                                    const SizedBox(width: 30),
+                                    Transform.rotate(
+                                      angle: 3.14159 / 2,
+                                      child: const Text(
+                                        'M',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Color(0xff9C4615),
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 2),
+                                    Text(
+                                      shopObjects.isNotEmpty
+                                          ? '${shopObjects[index].price}'
+                                          : '000',
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                FilledButton(
+                                    onPressed: _incrementCounter,
+                                    style: const ButtonStyle(
+                                      backgroundColor: MaterialStatePropertyAll(
+                                          Color(0xff236A26)),
+                                    ),
+                                    child: Row(children: [
+                                      const Text("Comprar"),
+                                      const SizedBox(width: 4),
+                                      Image(
+                                        //shopping cart icon
+                                        image: Image.asset(
+                                                "assets/icons/shopping_cart.png")
+                                            .image,
+                                        width: 24,
+                                      ),
+                                    ]))
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ])));
+  }
+}
