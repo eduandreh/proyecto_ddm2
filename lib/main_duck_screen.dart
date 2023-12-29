dfae10536e18f6479b532ffa90d993269a9ab866
zagarEmms <etarazg@gmail.com>
2023-12-29 23:21:30 +0100

    feat: add futureBuilder to wait for the query info, add weather request and assign its icon, swipe with mallards funcionality, callback with new duffy from shop

--- lib/main_duck_screen.dart
+++ lib/main_duck_screen.dart
@@ -7,7 +7,6 @@ import 'Duffy.dart';
 import 'firebase_manager.dart';
 
 class MainDuck extends StatefulWidget {
-  //needs to get duck object from login
   const MainDuck({super.key});
 
   @override
@@ -18,228 +17,275 @@ class _MainDuck extends State<MainDuck> {
   int _swipes = 0;
   List<String> backgroundImages = [];
   FirebaseManager fManager = FirebaseManager();
-  Duffy? _duffy;
+  String swipe = '';
+  late Future<Duffy> duffyFuture;
+  Duffy? _duffyWeather;
+  late Future<String> weatherFuture;
 
   @override
   void initState() {
+    duffyFuture = setDuck();
+    weatherFuture = setWeather();
     super.initState();
     saveAppOpenTime();
-    getDuck();
-    getCurrentWeather(48.856613, 2.352222); //need duck's location
+    //need duck's location
     getImages();
   }
 
-  void getDuck() async {
-    _duffy = await fManager.getDuck();
-    setState(() {});
+  Future<Duffy> setDuck() {
+    return Future(() => getDuck());
+  }
+
+  Future<Duffy> getDuck() async {
+    Duffy duck = await fManager.getDuck();
+    return duck;
+  }
+
+  Future<String> setWeather() {
+    if (_duffyWeather != null) {
+      return Future(() => getCurrentWeather(_duffyWeather?.location));
+    } else {
+      return Future(() => getCurrentWeather("London"));
+    }
   }
 
   void getImages() async {
     backgroundImages = await fManager.getImagesURL("/backgrounds");
-    setState(() {});
   }
 
   void _incrementSwipes() {
-    setState(() {
-      _swipes++;
-      if (_swipes == 10) {
-        fManager.updateDuckinessWithSwipes();
-        //fManager.updateDuckinessWithSwipes(duck.duckiness);
-        _swipes = 0;
-      }
-    });
+
+    _swipes = _swipes + 1;
+    if (_swipes == 10) {
+      fManager.updateMallards(1);
+
+      setState(() {
+        duffyFuture = setDuck();
+      });
+      _swipes = 0;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          centerTitle: true,
-          leading: IconButton(
-            onPressed: () {
-              signOut();
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SignInScreen()));
-            },
-            icon: const Icon(Icons.logout_rounded, color: Color(0xffDD8A29)),
-          ),
-          leadingWidth: 50,
-          title: Text(
-            _duffy != null ? _duffy!.location : "Location",
-            style: const TextStyle(fontSize: 18, color: Color(0xff7e7e7e)),
-          ),
-          actions: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(width: 30),
-                Transform.rotate(
-                  angle: 3.14159 / 2,
-                  child: const Text(
-                    'M',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xff9C4615),
-                    ),
+    return FutureBuilder(
+        future: duffyFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasData && backgroundImages.isNotEmpty) {
+            var duffy = snapshot.data;
+            _duffyWeather = duffy;
+            return Scaffold(
+              appBar: AppBar(
+                  centerTitle: true,
+                  bottom: const PreferredSize(preferredSize: Size.fromHeight(0), child: SizedBox()),
+                  leading: IconButton(
+                    onPressed: () {
+                      signOut();
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignInScreen()));
+                    },
+                    icon: const Icon(Icons.logout_rounded,
+                        color: Color(0xffDD8A29)),
                   ),
-                ),
-                const SizedBox(width: 2),
-                Text(
-                  _duffy == null ? "000" : _duffy!.coins.toString(),
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                  leadingWidth: 50,
+                  title: Text(
+                    duffy!.location,
+                    style:
+                        const TextStyle(fontSize: 18, color: Color(0xff7e7e7e)),
                   ),
-                ),
-                const SizedBox(width: 16),
-              ],
-            )
-          ]),
-      body: Center(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-            //weather icon
-            Visibility(
-                visible: _duffy == null ? true : false,
-                child: Image(
-                    image:
-                        Image.asset("assets/placeholders/duck_placeHolder.png")
-                            .image)),
-            Visibility(
-              visible: _duffy != null ? true : false,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      height: 0,
-                      child: IconButton(
-                        icon: const Icon(Icons.ac_unit_outlined),
-                        color: Colors.orange,
-                        onPressed: () {},
-                      ),
-                    ),
-
-                    const SizedBox(height: 32),
-                    //progress bar
-                    Stack(
-                      children: <Widget>[
-                        Align(
-                          alignment: Alignment.topCenter,
-                          child: SizedBox(
-                            width: 300,
-                            height: 30,
-                            child: LinearProgressIndicator(
-                              value: _duffy == null ? 1 : _duffy!.duckiness / 100,
-                              backgroundColor: Colors.grey[300],
-                              valueColor: const AlwaysStoppedAnimation<Color>(
-                                  Color(0xffDD8A29)),
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(30)),
+                  actions: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(width: 30),
+                        Transform.rotate(
+                          angle: 3.14159 / 2,
+                          child: const Text(
+                            'M',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xff9C4615),
                             ),
                           ),
                         ),
-                        const Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            "DUCKINESS",
-                            style: TextStyle(
-                                color: Color(0xff236A26),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 20),
+                        const SizedBox(width: 2),
+                        Text(
+                          duffy.coins.toString(),
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                           ),
-                        )
+                        ),
+                        const SizedBox(width: 16),
                       ],
-                    ),
-
-                    const SizedBox(height: 10),
-                    Text(_duffy == null ? 'Duffy' : _duffy!.name,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 32,
-                        )),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const Text("D",
-                            style: TextStyle(
-                                fontSize: 24, color: Color(0xff9C4615))),
-                        const SizedBox(width: 12),
-                        Text(_duffy == null ? '' : _duffy!.life.toString(),
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 28,
-                            )),
-                      ],
-                    ),
-
-                      Stack(
-                        children: <Widget>[
-                          Align(
-                            alignment: Alignment.topCenter,
-                            child: Image(
-                              //weather icon
-                              image: NetworkImage(backgroundImages[0]),
-                              width: 400,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 350,
-                            child: Align(
-                              alignment: Alignment.center,
-                              child: Image(
-                                //weather icon
-                                image: _duffy == null ? Image.asset("assets/placeholders/duckYellow.png").image : NetworkImage(_duffy!.outfit),
-                                width: 200,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                              width: 300,
-                              height: 300,
-                              child: GestureDetector(
-                                onPanUpdate: (details) {
-                                  if (details.delta.dx > 0) {
-                                    _incrementSwipes;
-                                  } else if (details.delta.dx < 0) {
-                                    _incrementSwipes;
-                                  }
-                                },
-                              ))
-                        ],
-                      ),
-
+                    )
                   ]),
-            )
-          ])),
-      bottomNavigationBar: BottomAppBar(
-        color: const Color(0xffBBDBBC),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            const Text("Let your Duffy be!",
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
-                )),
-            IconButton(
-              icon: const Icon(Icons.add_business),
-              color: Colors.orangeAccent,
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const ShopScreen()));
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+              body: Center(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                    //weather icon
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          FutureBuilder(
+                              future: weatherFuture,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData) {
+                                  final icon = snapshot.data.toString();
+
+                                  return Image.asset(
+                                    icon,
+                                    color: Colors.orange,
+                                    height: 30,
+                                  );
+                                } else {
+                                  return Image.asset(
+                                    "assets/weather/6sun_icon.png",
+                                    color: Colors.orange,
+                                  );
+                                }
+                              }),
+
+                          const SizedBox(height: 32),
+                          //progress bar
+                          Stack(
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment.topCenter,
+                                child: SizedBox(
+                                  width: 300,
+                                  height: 30,
+                                  child: LinearProgressIndicator(
+                                    value: duffy.duckiness / 100,
+                                    backgroundColor: Colors.grey[300],
+                                    valueColor:
+                                        const AlwaysStoppedAnimation<Color>(
+                                            Color(0xffDD8A29)),
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(30)),
+                                  ),
+                                ),
+                              ),
+                              const Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  "DUCKINESS",
+                                  style: TextStyle(
+                                      color: Color(0xff236A26),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 20),
+                                ),
+                              )
+                            ],
+                          ),
+
+                          const SizedBox(height: 10),
+                          Text(duffy.name,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 32,
+                              )),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              const Text("D",
+                                  style: TextStyle(
+                                      fontSize: 24, color: Color(0xff9C4615))),
+                              const SizedBox(width: 12),
+                              Text(duffy.life.toString(),
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 28,
+                                  )),
+                            ],
+                          ),
+
+                          Stack(
+                            children: <Widget>[
+                              Align(
+                                  alignment: Alignment.topCenter,
+                                  child: Visibility(
+                                      visible: backgroundImages.isNotEmpty
+                                          ? true
+                                          : false,
+                                      child:
+                                          Image.network(backgroundImages[0], width: 380))),
+                              SizedBox(
+                                height: 350,
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child:
+                                    //weather icon
+                                    Image.network(duffy.outfit, width: 200),
+                                ),
+                              ),
+                              SizedBox(
+                                  width: 300,
+                                  height: 300,
+                                  child: GestureDetector(
+                                    onPanUpdate: (details) {
+                                      if (details.delta.dx > 4) {
+                                        swipe = 'right';
+                                      } else if (details.delta.dx < 4) {
+                                        swipe = 'left';
+                                      }
+                                    },
+                                    onPanEnd: (details) {
+                                      if (swipe == 'right' || swipe == 'left') {
+                                        _incrementSwipes();
+                                      }
+                                    },
+                                  ))
+                            ],
+                          ),
+                        ])
+                  ])),
+              bottomNavigationBar: BottomAppBar(
+                color: const Color(0xffBBDBBC),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    const Text("Let your Duffy be!",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                        )),
+                    IconButton(
+                      icon: const Icon(Icons.add_business),
+                      color: Colors.orangeAccent,
+                      onPressed: () async {
+                        await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ShopScreen(
+                                  onDuckUpdated:(duffyUpt) {
+                                    if(duffyUpt != null ){
+                                      setState(() {
+                                        duffyFuture = Future.value(duffyUpt);
+                                      });
+                                    }
+                                  }
+                                )));
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return Stack(alignment: Alignment.center, children: <Widget>[
+              Container(
+                color: const Color(0xffBBDBBC),
+              ),
+              Image.asset("assets/placeholders/duck_placeHolder.png")
+            ]);
+          }
+        });
   }
 }
